c40b37dbcc1c05bdc3b372df279073fa5056e289
Vasilis Diakomanolis <b.diakomanolis@gmail.com>
2018-04-19 02:32:14 +0300

    lib initial draft

---
+++ lib/src/foo/foo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+ selector: 'my-foo',
+ template: '<h1>foo</h1>',
+})
+export class FooComponent {
+}
---
+++ lib/src/public_api.ts
@@ -0,0 +1,2 @@
+export * from './foo/foo.component';
+// asd

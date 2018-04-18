a8fec018c1275ffa244069b45e761d730be92cd8
Vasilis Diakomanolis <b.diakomanolis@gmail.com>
2018-04-19 02:32:32 +0300

    demo initial draft

---
+++ demo/app/app.component.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed, async } from '@angular/core/testing';
+import { MyLibModule } from 'ngx-timeago';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        MyLibModule.forRoot()
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+
+});

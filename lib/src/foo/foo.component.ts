7bbd36ebff4573c63cacce7d11abb235952fd989
Vasilis Diakomanolis <b.diakomanolis@gmail.com>
2018-04-20 04:37:54 +0300

    chore(app): remove useless files

--- lib/src/foo/foo.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
- selector: 'my-foo',
- template: '<h1>foo</h1>',
-})
-export class FooComponent {
-}
--- lib/src/foo/foo.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-
-describe('FooComponent', () => {
-    it('can load instance', () => {
-        expect(true).toBeTruthy();
-    });
-});

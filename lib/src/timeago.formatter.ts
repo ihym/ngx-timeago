ea35d1974f8931c23403dc86ddb473e8633101b4
Vasilis Diakomanolis <b.diakomanolis@gmail.com>
2018-04-28 19:33:36 +0300

    fix(store): remove store

--- lib/src/timeago.formatter.ts
+++ lib/src/timeago.formatter.ts
@@ -1,16 +1,16 @@
-import {Injectable} from '@angular/core';
+import {Injectable, Optional} from '@angular/core';
 import {TimeagoIntl} from './timeago.intl';
 
 type Unit = 'second' | 'minute' | 'hour' | 'day' | 'week' | 'month' | 'year';
 
-type Suffix = 'ago' | 'from now';
+type Suffix = 'ago' | 'from now' | '';
 
 export type StringOrFn = ((value: number, millisDelta:  number) => string) | string;
 
 export type NumberArray = [ string, string, string, string, string, string, string, string, string, string ];
 
 export abstract class TimeagoFormatter {
-  constructor(public intl: TimeagoIntl) {}
+  constructor(@Optional() public intl: TimeagoIntl) {}
 
   abstract format(value: number, unit: string, suffix: string, epochMillis?: number): string;
 }
@@ -30,46 +30,46 @@ export class TimeagoCustomFormatter extends TimeagoFormatter {
   format(value: number, unit: Unit, suffix: Suffix, epochMillis: number) {
     // convert weeks to days if strings don't handle weeks
     const now = Date.now();
-    if (unit === 'week' && !this.intl.strings.week && !this.intl.strings.weeks) {
+    if (unit === 'week' && !this.intl.week && !this.intl.weeks) {
       const days = Math.round(Math.abs(epochMillis - now) / (1000 * 60 * 60 * 24));
       value = days;
       unit = 'day';
     }
 
     // create a normalize function for given value
-    const normalize = this.normalizeFn(value, now - epochMillis, this.intl.strings.numbers);
+    const normalize = this.normalizeFn(value, now - epochMillis, this.intl.numbers);
 
     // The eventual return value stored in an array so that the wordSeparator can be used
     let dateString: string[] = [];
 
     // handle prefixes
-    if (suffix === 'ago' && this.intl.strings.prefixAgo) {
-      dateString.push(normalize(this.intl.strings.prefixAgo));
+    if (suffix === 'ago' && this.intl.prefixAgo) {
+      dateString.push(normalize(this.intl.prefixAgo));
     }
-    if (suffix === 'from now' && this.intl.strings.prefixFromNow) {
-      dateString.push(normalize(this.intl.strings.prefixFromNow));
+    if (suffix === 'from now' && this.intl.prefixFromNow) {
+      dateString.push(normalize(this.intl.prefixFromNow));
     }
 
     // Handle Main number and unit
     const isPlural = value > 1;
     if (isPlural) {
-      const stringFn: StringOrFn = this.intl.strings[unit + 's'] || this.intl.strings[unit] || '%d ' + unit;
+      const stringFn: StringOrFn = this.intl[unit + 's'] || this.intl[unit] || '%d ' + unit;
       dateString.push(normalize(stringFn));
     } else {
-      const stringFn: StringOrFn = this.intl.strings[unit] || this.intl.strings[unit + 's'] || '%d ' + unit;
+      const stringFn: StringOrFn = this.intl[unit] || this.intl[unit + 's'] || '%d ' + unit;
       dateString.push(normalize(stringFn));
     }
 
     // Handle Suffixes
-    if (suffix === 'ago' && this.intl.strings.suffixAgo) {
-      dateString.push(normalize(this.intl.strings.suffixAgo));
+    if (suffix === 'ago' && this.intl.suffixAgo) {
+      dateString.push(normalize(this.intl.suffixAgo));
     }
-    if (suffix === 'from now' && this.intl.strings.suffixFromNow) {
-      dateString.push(normalize(this.intl.strings.suffixFromNow));
+    if (suffix === 'from now' && this.intl.suffixFromNow) {
+      dateString.push(normalize(this.intl.suffixFromNow));
     }
 
     // join the array into a string and return it
-    const wordSeparator = typeof this.intl.strings.wordSeparator === 'string' ? this.intl.strings.wordSeparator : ' ';
+    const wordSeparator = typeof this.intl.wordSeparator === 'string' ? this.intl.wordSeparator : ' ';
     return dateString.join(wordSeparator);
   };
 

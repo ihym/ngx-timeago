3a672dff77557fc7a48b99371668f551f51ab9a2
Vasilis Diakomanolis <b.diakomanolis@gmail.com>
2018-05-18 03:09:38 +0300

    chore(demo): provide more language options

--- demo/app/child/child.component.ts
+++ demo/app/child/child.component.ts
@@ -2,6 +2,8 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { TimeagoIntl } from 'ngx-timeago';
 import {strings as englishStrings} from 'ngx-timeago/language-strings/en';
 import {strings as englishShortStrings} from 'ngx-timeago/language-strings/en-short';
+import {strings as frenchStrings} from 'ngx-timeago/language-strings/fr';
+import {strings as frenchShortStrings} from 'ngx-timeago/language-strings/fr-short';
 
 @Component({
   selector: 'app-child',
@@ -22,6 +24,8 @@ export class ChildComponent {
     switch (lang) {
       case 'en': this.intl.strings = englishStrings; break;
       case 'en-short': this.intl.strings = englishShortStrings; break;
+      case 'fr': this.intl.strings = frenchStrings; break;
+      case 'fr-short': this.intl.strings = frenchShortStrings; break;
       default: break;
     }
     this.intl.changes.next();

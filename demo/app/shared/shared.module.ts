45cee5b0d3924069668ed0ae6cc8c93155f320e7
frankwebdevelopment <frankwebdevelopment@gmail.com>
2020-08-07 10:18:54 +0200

    Update to angular 10

--- demo/app/shared/shared.module.ts
+++ demo/app/shared/shared.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {
-  MatCheckboxModule,
-  MatCardModule,
-  MatSelectModule,
-} from '@angular/material';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   imports: [

ce63e9c1c8d890cecf64e3df3314260890cd82dc
Dennis Blokland <dennishoornaar@live.nl>
2020-12-18 11:13:23 +0100

    Create nl-short.ts

---
+++ lib/src/language-strings/nl-short.ts
@@ -0,0 +1,20 @@
+import { IL10nsStrings } from '../timeago.intl';
+
+export const strings: IL10nsStrings = {
+  prefixAgo: null,
+  prefixFromNow: null,
+  suffixAgo: '',
+  suffixFromNow: '',
+  seconds: '1m',
+  minute: '1m',
+  minutes: '%dm',
+  hour: '1u',
+  hours: '%du',
+  day: '1d',
+  days: '%dd',
+  month: '1ma',
+  months: '%dma',
+  year: '1jr',
+  years: '%djr',
+  wordSeparator: ' ',
+};

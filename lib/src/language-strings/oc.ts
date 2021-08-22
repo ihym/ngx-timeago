fdd26380b3aa44d48d4a61c3084be4f2bbf5aea6
Mejans <61360811+Mejans@users.noreply.github.com>
2021-08-22 20:57:10 +0200

    Create oc.ts

---
+++ lib/src/language-strings/oc.ts
@@ -0,0 +1,21 @@
+import {IL10nsStrings} from '../timeago.intl';
+
+export const strings: IL10nsStrings = {
+  prefixAgo: 'fa',
+  prefixFromNow: 'd’aqui',
+  suffixAgo: null,
+  suffixFromNow: null,
+  seconds: 'mens d’1 minuta',
+  minute: '1 minuta',
+  minutes: 'unas %d minutas',
+  hour: '1 ora',
+  hours: 'unas %d oras',
+  day: '1 jorn',
+  days: '%d jorns',
+  month: 'aproximativament un mes',
+  months: '%d meses',
+  year: 'aproximativament un an',
+  years: '%d ans',
+};
+
+
